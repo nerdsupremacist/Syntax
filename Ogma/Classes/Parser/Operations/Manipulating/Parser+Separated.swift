d51cf36bf8f596f426be5ec7500d92e42bc22c61
Mathias Quintero <me@quintero.io>
2019-04-24 10:40:36 +0200

    Adding allowsEmpty flag to separated by

--- Ogma/Classes/Parser/Operations/Manipulating/Parser+Separated.swift
+++ Ogma/Classes/Parser/Operations/Manipulating/Parser+Separated.swift
@@ -10,8 +10,16 @@ import Foundation
 
 extension Parser {
     
-    // [Self]([Separator][Self])*
-    public func separated(by separator: Token) -> AnyParser<Token, [Output]> {
+    // [Self]([Separator][Self])* when empty is not allowed
+    // [Self]([Separator][Self])*? when empty is allowed
+    public func separated(by separator: Token, allowsEmpty: Bool = false) -> AnyParser<Token, [Output]> {
+
+        if allowsEmpty {
+            return separated(by: separator, allowsEmpty: false)
+                .optional()
+                .map { $0 ?? [] }
+        }
+
         let subsequent = separator && self
         let parser = self && subsequent*
         return parser.map { [$0] + $1 }
