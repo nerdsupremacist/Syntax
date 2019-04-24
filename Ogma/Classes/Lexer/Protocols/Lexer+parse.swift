883547b5154bfae01750a6290b3371305724616f
Mathias Quintero <me@quintero.io>
2019-04-24 13:28:00 +0200

    Adding Lexer+parse helper

---
+++ Ogma/Classes/Lexer/Protocols/Lexer+parse.swift
@@ -0,0 +1,17 @@
+//
+//  Lexer+parse.swift
+//  Ogma
+//
+//  Created by Mathias Quintero on 4/24/19.
+//
+
+import Foundation
+
+extension LexerProtocol {
+
+    public static func parse<P: Parsable>(input: String, of type: P.Type = P.self) throws -> P where P.Token == Token {
+        let tokens = try tokenize(input: input)
+        return try type.init(tokens: tokens)
+    }
+
+}
