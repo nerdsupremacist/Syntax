700df5916ab2187e996a0586570623f054f2c54f
Mathias Quintero <me@quintero.io>
2019-04-25 14:45:07 +0200

    Adding comment token generators

---
+++ Ogma/Classes/Lexer/StandardGenerators/MultiLineCommentTokenGenerator.swift
@@ -0,0 +1,34 @@
+//
+//  MultiLineCommentTokenGenerator.swift
+//  Ogma
+//
+//  Created by Mathias Quintero on 4/25/19.
+//
+
+import Foundation
+
+public struct MultiLineComment: TokenProtocol {
+    public let text: String
+}
+
+public struct MultiLineCommentTokenGenerator: RegexTokenGeneratorProtocol {
+    public typealias Token = MultiLineComment
+
+    let prefixPattern: String
+    let postfixPattern: String
+
+    public var pattern: String {
+        return "\(prefixPattern)[ \t\n]+([^\(postfixPattern)]*)[ \t\n]+\(postfixPattern)"
+    }
+
+    public let group: Int = 1
+
+    public init(prefixPattern: String, postfixPattern: String) {
+        self.prefixPattern = prefixPattern
+        self.postfixPattern = postfixPattern
+    }
+
+    public func token(from matched: String) throws -> MultiLineComment? {
+        return MultiLineComment(text: matched)
+    }
+}
---
+++ Ogma/Classes/Lexer/StandardGenerators/SingleLineCommentTokenGenerator.swift
@@ -0,0 +1,32 @@
+//
+//  SingleLineCommentTokenGenerator.swift
+//  Ogma
+//
+//  Created by Mathias Quintero on 4/25/19.
+//
+
+import Foundation
+
+public struct SingleLineComment: TokenProtocol {
+    public let text: String
+}
+
+public struct SingleLineCommentTokenGenerator: RegexTokenGeneratorProtocol {
+    public typealias Token = SingleLineComment
+
+    let prefixPattern: String
+
+    public var pattern: String {
+        return "\(prefixPattern)[ \t\n]+([^\\n]*)"
+    }
+
+    public let group: Int = 1
+
+    public init(prefixPattern: String) {
+        self.prefixPattern = prefixPattern
+    }
+
+    public func token(from matched: String) throws -> SingleLineComment? {
+        return SingleLineComment(text: matched)
+    }
+}
