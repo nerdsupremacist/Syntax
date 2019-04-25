f3b4186b9ab0ba4d844dd79a637895a1e97197be
Mathias Quintero <me@quintero.io>
2019-04-25 14:44:27 +0200

    Adding ignore to tokengenerator

--- Ogma/Classes/Lexer/Operatios/TokenGenerator+map.swift
+++ Ogma/Classes/Lexer/Operatios/TokenGenerator+map.swift
@@ -10,15 +10,19 @@ import Foundation
 extension TokenGenerator {
 
     /// Map a Token Generator to create Tokens of another Type
-    public func map<T: TokenProtocol>(_ transform: @escaping (Token) throws -> T) -> AnyTokenGenerator<T> {
+    public func map<T: TokenProtocol>(_ transform: @escaping (Token) throws -> T?) -> AnyTokenGenerator<T> {
         return MappedTokenGenerator(source: self, transform: transform).any()
     }
 
+    public func ignore<T: TokenProtocol>() -> AnyTokenGenerator<T> {
+        return map { _ in nil }
+    }
+    
 }
 
 private struct MappedTokenGenerator<Token: TokenProtocol, Source: TokenGenerator>: TokenGenerator {
     let source: Source
-    let transform: (Source.Token) throws -> Token
+    let transform: (Source.Token) throws -> Token?
 
     func take(text: String) throws -> Generated<Token> {
         return try source.take(text: text).map(transform)
