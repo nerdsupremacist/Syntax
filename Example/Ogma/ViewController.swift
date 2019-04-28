dd31744886253d9f166ade290be40d92febbd3a2
Mathias Quintero <me@quintero.io>
2019-04-29 01:44:26 +0200

    Adding text view to view controller

--- Example/Ogma/ViewController.swift
+++ Example/Ogma/ViewController.swift
@@ -10,15 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var textView: UITextView!
+
+    var timer: Timer?
+    var defaultAttributes: [NSAttributedString.Key : Any]?
+
+    deinit {
+        timer?.invalidate()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+
+        defaultAttributes = textView.typingAttributes
+        evaluateInput()
     }
 
 }
 
+extension ViewController: UITextViewDelegate {
+
+    func textViewDidChange(_ textView: UITextView) {
+        timer?.invalidate()
+
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { [unowned self] _ in
+            self.evaluateInput()
+        }
+    }
+
+    func textView(_ textView: UITextView,
+                  shouldInteractWith url: URL,
+                  in characterRange: NSRange,
+                  interaction: UITextItemInteraction) -> Bool {
+
+        guard let json = textView
+            .attributedText
+            .attribute(.annotationValue,
+                       at: characterRange.location,
+                       effectiveRange: nil) as? JSON else { return true }
+
+        present(json: json)
+        return false
+    }
+
+}
+
+extension ViewController {
+
+    private func evaluateInput() {
+        guard let input = textView.text else { return }
+        let selectedRange = textView.selectedRange
+
+        let string = try! JSON
+            .annotate(input, using: JSON.Lexer.self)
+            .string(attributes: defaultAttributes) { json in
+                return [
+                    .link: URL(string: "json://")!,
+                ]
+            }
+
+        textView.attributedText = string
+        textView.selectedRange = selectedRange
+    }
+
+}
