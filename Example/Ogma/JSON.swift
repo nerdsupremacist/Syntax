81bbaf07939a53ca9c7138838ccd65384d663f69
Mathias Quintero <me@quintero.io>
2019-04-29 01:44:02 +0200

    Adding dynamic member lookup and string helpers to json

--- Example/Ogma/JSON.swift
+++ Example/Ogma/JSON.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+@dynamicMemberLookup
 public enum JSON {
     case dictionary([String : JSON])
     case array([JSON])
@@ -16,4 +17,58 @@ public enum JSON {
     case string(String)
     case bool(Bool)
     case null
+
+    public subscript(dynamicMember member: String) -> JSON? {
+        if case .array(let array) = self,
+            let number = Int(member),
+            number < array.count {
+            
+            return array[number]
+        }
+
+        guard case .dictionary(let dictionary) = self else { return nil }
+        return dictionary[member]
+    }
+}
+
+extension JSON {
+
+    var valueDescription: String {
+        switch self {
+        case .dictionary:
+            return "Object"
+        case .array:
+            return "Array"
+        case .int(let int):
+            return String(int)
+        case .double(let double):
+            return String(double)
+        case .string(let string):
+            return "\"\(string)\""
+        case .bool(let bool):
+            return String(bool)
+        case .null:
+            return "null"
+        }
+    }
+
+    var typeTitle: String {
+        switch self {
+        case .dictionary:
+            return "Object"
+        case .array:
+            return "Array"
+        case .int:
+            return "Integer"
+        case .double:
+            return "Double"
+        case .string:
+            return "String"
+        case .bool:
+            return "Boolean"
+        case .null:
+            return "null"
+        }
+    }
+
 }
