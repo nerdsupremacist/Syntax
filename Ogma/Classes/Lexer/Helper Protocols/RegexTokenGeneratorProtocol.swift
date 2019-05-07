23edda4835c10da12581e9fa528ad0d976018a44
Mathias Quintero <me@quintero.io>
2019-05-07 20:05:58 +0200

    Reverting empty group change

--- Ogma/Classes/Lexer/Helper Protocols/RegexTokenGeneratorProtocol.swift
+++ Ogma/Classes/Lexer/Helper Protocols/RegexTokenGeneratorProtocol.swift
@@ -48,7 +48,11 @@ extension RegexTokenGeneratorProtocol {
     func token(in match: NSTextCheckingResult, for text: String) throws -> Token? {
         let groupRange = match.range(at: group)
 
-        let matched = Range(groupRange, in: text).map { String(text[$0]) } ?? ""
+        guard let range = Range(groupRange, in: text) else {
+            throw LexerError.noMatchFound(text, pattern: pattern)
+        }
+
+        let matched = String(text[range])
 
         do {
             return try token(from: matched)
