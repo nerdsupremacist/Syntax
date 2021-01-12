e67a0253416df7fb49eadec5162b0f145bbf9914
Mathias Quintero <me@quintero.io>
2021-01-12 23:47:53 +0100

    Adding boolean literal parser

---
+++ Sources/Syntax/Implementations/Common/BooleanLiteral.swift
@@ -0,0 +1,36 @@
+
+import Foundation
+
+public struct BooleanLiteral: Parser {
+    public static var kind: Kind = .booleanLiteral
+
+    let trueValues: Set<String>
+    let falseValues: Set<String>
+
+    public init(true trueValues: Set<String> = ["true"],
+                false falseValues: Set<String> = ["false"]) {
+
+        self.trueValues = trueValues
+        self.falseValues = falseValues
+    }
+
+    public var body: AnyParser<Bool> {
+        Leaf {
+            Either {
+                Either(trueValues) { token in
+                    Word(token).map(to: true)
+                }
+
+                Either(falseValues) { token in
+                    Word(token).map(to: false)
+                }
+            }
+        }
+    }
+}
+
+extension Kind {
+
+    public static let booleanLiteral: Kind = "boolean.literal"
+
+}
