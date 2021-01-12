16ecdfffdf3057d6081aa337914919b589817b16
Mathias Quintero <me@quintero.io>
2021-01-12 21:53:24 +0100

    Making group an internal parser

--- Sources/Syntax/Implementations/Structure/Group.swift
+++ Sources/Syntax/Implementations/Structure/Group.swift
@@ -2,13 +2,25 @@
 import Foundation
 
 public struct Group<Content : Parser>: Parser {
-    private let content: Content
+    private let content: InternalParser
 
     public init(@ParserBuilder content: () -> Content) {
-        self.content = content()
+        self.content = content().internalParser()
     }
 
     public var body: AnyParser<Content.Output> {
-        content
+        return neverBody()
     }
+}
+
+extension Group: InternalParser {
+
+    func prefixes() -> Set<String> {
+        return content.prefixes()
+    }
+
+    func parse(using scanner: Scanner) throws {
+        try content.parse(using: scanner)
+    }
+
 }
