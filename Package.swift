1909a0f23bfa2206db7086cfae026faa7e9aa29c
Mathias Quintero <me@quintero.io>
2021-02-06 20:25:31 +0100

    updating syntax tree

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
                  targets: ["Syntax"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nerdsupremacist/SyntaxTree.git", .branch("main")),
+        .package(url: "https://github.com/nerdsupremacist/SyntaxTree.git", from: "0.1.0"),
     ],
     targets: [
         .target(name: "Syntax",
