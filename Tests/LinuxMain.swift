2adf8dc3f479babbc002420683fb527956873ab0
Mathias Quintero <me@quintero.io>
2021-02-07 00:13:22 +0100

    using test autodiscovery

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,7 @@
-import XCTest
-
-import TextMateSplashTests
-
-var tests = [XCTestCaseEntry]()
-tests += TextMateSplashTests.allTests()
-XCTMain(tests)
+// A LinuxMain.swift file is no longer needed since `swift test --enable-test-discovery` is possible
+// Provide an error message when testing on Linux with no automatic test discovery
+#error("""
+    -----------------------------------------------------
+    Please test with `swift test --enable-test-discovery`
+    -----------------------------------------------------
+    """)
