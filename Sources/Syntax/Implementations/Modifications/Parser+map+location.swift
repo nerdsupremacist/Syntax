359f8f44711b008a14ffe17130f2762b256e70a7
Mathias Quintero <me@quintero.io>
2022-06-27 17:43:48 +0100

    Fixing mapWithLocation

--- Sources/Syntax/Implementations/Modifications/Parser+map+location.swift
+++ Sources/Syntax/Implementations/Modifications/Parser+map+location.swift
@@ -1,17 +1,17 @@
 
 import Foundation
-import SyntaxTree
+@_exported import SyntaxTree
 
 extension Parser {
 
     public func mapWithLocation<T>(_ transform: @escaping (Output, Range<Location>) throws -> T) -> AnyParser<T> {
-        return MappedParser<Self, T>(parser: internalParser(),
-                                     transform: transform).eraseToAnyParser()
+        return ParserWithLocation<Self, T>(parser: internalParser(),
+                                           transform: transform).eraseToAnyParser()
     }
 
 }
 
-private struct MappedParser<Source: Parser, Output>: Parser {
+private struct ParserWithLocation<Source: Parser, Output>: Parser {
     let id = UUID()
     fileprivate let parser: InternalParser
     fileprivate let transform: (Source.Output, Range<Location>) throws -> Output
@@ -21,16 +21,18 @@ private struct MappedParser<Source: Parser, Output>: Parser {
     }
 }
 
-extension MappedParser: InternalParser {
+extension ParserWithLocation: InternalParser {
     func prefixes() -> Set<String> {
         return parser.prefixes()
     }
 
     func parse(using scanner: Scanner) throws {
         scanner.enterNode()
+        scanner.enterNode()
         try scanner.parse(using: parser)
         scanner.exitNode()
         let location = scanner.locationOfNode()
+        scanner.exitNode()
         let transformed: Output
         if Source.Output.self != Void.self {
             let output = try scanner.pop(of: Source.Output.self)
